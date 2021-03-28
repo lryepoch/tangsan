4369cfedca9b0c8251995f458e48768fff1798c9
lryepoch <lryepoch@qq.com>
2021-03-28 19:22:51 +0800

    2021-3-28

---
+++ music/music-server/src/main/java/com/lryepoch/MusicServerApplication.java
@@ -0,0 +1,11 @@
+package com.lryepoch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MusicServerApplication.class, args);
+    }
+}
